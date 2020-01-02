3884a08bc9de05e40e68c17e4a2589f33efbd3a5
C Beach <cbeach@unicon.net>
2020-01-02 03:34:39 -0600

    Setup gradle

---
+++ src/main/java/org/ofm/utils/Driver.java
@@ -0,0 +1,12 @@
+package org.ofm.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Driver {
+	private static Logger LOGGER = LogManager.getLogger(Driver.class);
+
+	public static void main(String[] args) throws Exception {
+		LOGGER.info("We're Alive!!");	
+	}
+}
